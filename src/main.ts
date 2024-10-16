befcc74bc1a8f55f7bd3c760412e28b7de910424
scso-ucsc <scso@ucsc.edu>
2024-10-15 19:24:06 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,21 @@
 import "./style.css";
 
-const APP_NAME = "MY STICKER SKETCHER PROTOTYPE";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
-document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+//Step 1: Initial non-interactive UI layout
+function createTitle(titleName: string) {
+  const appTitle = document.createElement("h1"); //Creating title element
+  appTitle.textContent = titleName;
+  app.appendChild(appTitle);
+}
+
+function createCanvas(inputWidth: number, inputHeight: number) {
+  const canvas = document.createElement("canvas");
+  canvas.id = "canvasMain"; //Adding ID so that style.css can access it
+  canvas.width = inputWidth;
+  canvas.height = inputHeight;
+  app.appendChild(canvas);
+}
+
+createTitle("MY STICKER SKETCHPAD");
+createCanvas(256, 256); //Creating a 256 x 256 canvas
